8e851853e8eeac06747708092efea3310d53feab
nazamateo <nazamateo@gmail.com>
2022-07-21 20:41:09 +0800

    Display API request responses at static#home view

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,13 @@
 class StaticController < ApplicationController
+
   def home
+    location_client = Geolocation::Api::Client.new
+    @location = location_client.get_location
+
+    advise_client = Advise::Api::Client.new
+    @random_advise = advise_client.get_random_advise
+
+    country_client = Country::Api::Client.new
+    @countries = client.get_all_countries.sort_by{|c| c['continents']}
   end
 end
