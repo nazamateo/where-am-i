e3288661d8bef50ecbbfe7427322f0d9f944668e
nazamateo <nazamateo@gmail.com>
2022-07-21 20:48:41 +0800

    Use API wrappers in controller static#show

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,6 +12,12 @@ class StaticController < ApplicationController
   end
 
   def show
+    covid_data_client = Covid::Api::Client.new
+    @covid_data = covid_data_client.get_covid_data_by_country(params[:country].gsub(/\s+/, '-'))
+
+    country_client = Country::Api::Client.new
+    @country = country_client.get_country_details(params[:country].gsub(/\s+/, '%20'))
+
   end
-  
+
 end
